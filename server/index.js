1f27403ca2a96f7f249f81f59280afcb61d80997
Padmanabha Roy <padmanabharoy@Padmanabhas-MacBook-Air.local>
2026-04-12 09:40:22 +0530

    Fix Railway deployment: add start script to root and fix .env path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: './server/.env' });
 
 const app = express();
 
