1397743fce7cd50994fd241a993d298761ab2c06
Padmanabha Roy <padmanabharoy@Padmanabhas-MacBook-Air.local>
2026-04-21 15:42:02 +0530

    revert: change Prokerala chart language back to English (en)

--- server/index.cjs
+++ server/index.cjs
@@ -91,7 +91,7 @@ app.post('/api/kundali', async (req, res) => {
                 chart_style: 'east-indian',
                 datetime: datetime,
                 coordinates: `${lat},${lon}`,
-                la: 'bn'
+                la: 'en'
             },
             headers: {
                 'Authorization': `Bearer ${token}`,
