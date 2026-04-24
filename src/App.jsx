f4e209acd3d751c97f31aacb682446e93e4723d8
Padmanabha Roy <padmanabharoy@Padmanabhas-MacBook-Air.local>
2026-04-24 11:58:47 +0530

    feat: add Instagram Feed carousel section below FAQs with glassmorphism cards

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ import Testimonials from './components/Testimonials';
 import Hero from './components/Hero';
 import Kundali from './components/Kundali';
 import Pricing from './components/Pricing';
+import InstagramFeed from './components/InstagramFeed';
 import LegalModal from './components/LegalModal';
 import StarfieldBg from './components/StarfieldBg';
-import RainEffect from './components/RainEffect';
 
 import logo from './assets/logo.png';
 import zodiacWheel from './assets/zodiac-wheel.png';
@@ -163,7 +163,6 @@ If you have any questions regarding this Privacy Policy or how your data is hand
       onDragStart={(e) => e.preventDefault()}
     >
       <StarfieldBg />
-      <RainEffect />
       {/* Progress Bar */}
       <motion.div
         className="fixed top-0 left-0 right-0 h-1 z-50 origin-left"
@@ -346,6 +345,9 @@ If you have any questions regarding this Privacy Policy or how your data is hand
 
       {/* FAQs Section */}
       <FAQs />
+
+      {/* Instagram Feed Section */}
+      <InstagramFeed />
 
       <Testimonials />
       <Footer onOpenLegal={openLegalModal} />
