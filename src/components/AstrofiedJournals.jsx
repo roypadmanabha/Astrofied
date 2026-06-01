d4ddbb4cbefd4189ca39697202d1eba245179925
Padmanabha Roy <padmanabharoy@Padmanabhas-MacBook-Air.local>
2026-06-01 11:12:02 +0530

    style: set legal disclaimer opacity to 60%

--- src/components/AstrofiedJournals.jsx
+++ src/components/AstrofiedJournals.jsx
@@ -351,7 +351,7 @@ const AstrofiedJournals = () => {
             <p className="text-justify text-sm md:text-base leading-relaxed mb-4">
               Welcome to Astrofied Journals! Explore our articles, posts, and in-depth analyses on various topics of astrology. Feel free to download, share, and gain a deeper understanding of astrological concepts. Don't hesitate to contact us if you have any questions or queries.
             </p>
-            <p className="text-justify text-[10px] md:text-xs leading-relaxed font-mulish font-normal text-[#3d0000]">
+            <p className="text-justify text-[10px] md:text-xs leading-relaxed font-mulish font-normal text-[#3d0000] opacity-60">
               Note: All content within Astrofied Journals is provided strictly for personal reading and educational purposes. Unauthorised reproduction, distribution, resale, or commercial use of these materials is strictly prohibited under Indian Copyright Act. Astrofied reserves all intellectual property rights, and any infringement will be subject to strict legal action.
             </p>
           </div>
