7ab33d1a7578a6c949d1c424b57334ab22f31f24
Padmanabha Roy <padmanabharoy@Padmanabhas-MacBook-Air.local>
2026-06-01 21:45:52 +0530

    feat: add SEO meta tags, json-ld schema, and setup auto sitemap generation

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://astrofied.netlify.app'
+    })
   ],
   // Relative base path ensures compatibility across all hosting platforms (Netlify, GH Pages, etc.)
   base: './',
